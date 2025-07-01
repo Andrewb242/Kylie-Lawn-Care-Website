3454fda4201ca80d60d760024a80b370b624aa55
Andrew Boob <AndrewB2277@outlook.com>
2025-07-01 17:02:38 -0400

    Worked on the services page about 6 hours 40 min

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,25 +1,16 @@
 import React from "react";
 import "./Home.css";
-import serviceData from "../Data/serviceData.json";
 
 export default function Home() {
-  const { services } = serviceData;
-
   return (
     <div className="home-container">
       <div className="home-cover-photo">
-        <div className="home-logo-container">
-          <img className="home-logo" src="logo-square.svg" alt="Logo Square" />
+        <div className="home-slogan-container">
+          <h1>Precision Landscaping. Lasting Beauty.</h1>
         </div>
       </div>
       <div className="home-about-container">
         <h2>About Us</h2>
-      </div>
-      <div className="home-services-container">
-        <h1>Our Services</h1>
-        {services.map((service, index) => (
-          <p key={index}>{service}</p>
-        ))}
       </div>
     </div>
   );
--- src/Pages/Service.jsx
+++ src/Pages/Service.jsx
@@ -1,5 +1,35 @@
 import React from "react";
+import "./Service.css";
+import serviceData from "../Data/serviceData.json";
 
 export default function Service() {
-  return <div>Service</div>;
+  const { services, serviceArea } = serviceData;
+
+  return (
+    <div className="service-container">
+      <div className="service-s-container">
+        <h1>Our Services</h1>
+        {services.map((service, index) => (
+          <p key={index}>{service}</p>
+        ))}
+      </div>
+
+      <div className="service-sa-container">
+        {serviceArea && (
+          <>
+            <h2>Service Area</h2>
+            <p>{serviceArea}</p>
+          </>
+        )}
+      </div>
+
+      <div className="cta-section">
+        <h2>Get in Touch</h2>
+        <p>
+          Contact us today to schedule your landscaping service or get a free
+          quote!
+        </p>
+      </div>
+    </div>
+  );
 }
